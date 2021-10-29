bb2bcf189450b973e898458ba92071ab8aedbe84
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-10-29 13:43:19 +0600

    start work on add tour offer

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <h2>header</h2>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+                <Container>
+                    <Navbar.Brand href="#home">Bangladesh Tourism</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto text-decoration-none">
+                            <Link className="text-decoration-none text-light mx-2" to='/home'>Home</Link>
+                            <Link className="text-decoration-none text-light mx-2" to='/tourismoffer'>Tourism Offer</Link>
+                            <Link className="text-decoration-none text-light mx-2" to='/addtouroffer'>Add Tour Offer</Link>
+                        </Nav>
+                        <Nav >
+                            <Nav.Link href="#deets">user name</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
