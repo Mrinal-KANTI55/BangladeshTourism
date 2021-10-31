76352acb165eca2b9681ae04ad005b64820555ed
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-10-31 20:51:44 +0600

    update some step

--- src/Pages/LogIn/PrivateRoute/PrivateRoute.js
+++ src/Pages/LogIn/PrivateRoute/PrivateRoute.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 import { Redirect, Route } from 'react-router';
 import useAuth from '../../../Hooks/UseAuth/UseAuth';
 
 const PrivateRoute = ({ children, ...rest }) => {
-    const { user,isLoading } = useAuth();
-    if(isLoading){
-        return <h3>Loading...</h3>
+    const { user, isLoading } = useAuth();
+    if (isLoading) {
+        return <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </Spinner>
     }
     return (
         <Route
             {...rest}
             render={({ location }) =>
                 user.email ? children : <Redirect
-                        to={{
-                            pathname: "/login",
-                            state: { from: location }
-                        }}
-                    ></Redirect>
+                    to={{
+                        pathname: "/login",
+                        state: { from: location }
+                    }}
+                ></Redirect>
             }
         >
 
